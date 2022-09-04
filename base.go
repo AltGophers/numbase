88dfa86113d5803f57395131358e1cb1294c1018
realisijola <ayodejiisijola@gmail.com>
2022-09-04 14:13:47 +0100

    changed the different int type used and adopted a single int type for numerical variables and parameters

--- base.go
+++ base.go
@@ -7,27 +7,27 @@ import (
 )
 
 // toBase10 converts a number in a specified base to decimal (base 10)
-func toBase10(base int8, number int64) (int64, error) {
+func toBase10(base int, number int) (int, error) {
 	index, answer := 0, 0.0
 	inputNum := number
 	for number != 0 {
 		currentNum := number % 10
-		if int8(currentNum) >= base {
-			return 0, fmt.Errorf("Error: %d not in specified base: %d", inputNum, base)
+		if currentNum >= base {
+			return 0, fmt.Errorf("error: %d not in specified base: %d", inputNum, base)
 		}
 
 		number /= 10
 		answer += float64(currentNum) * math.Pow(float64(base), float64(index))
 		index++
 	}
-	return int64(answer), nil
+	return int(answer), nil
 }
 
-// fromAnyBasetoAnyBase converts a number in a specified base to a desired base
-func fromAnyBasetoAnyBase(base int8, number int64, desiredBase int) (int, error) {
+ // fromAnyBasetoAnyBase converts a number in a specified base to a desired base
+func fromAnyBasetoAnyBase(base int, number int, desiredBase int) (int, error) {
 
 	if base != 10 {
-		numBase10, err := toBase10(base, number)
+		numBase10, err := toBase10(base, int(number))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -38,8 +38,8 @@ func fromAnyBasetoAnyBase(base int8, number int64, desiredBase int) (int, error)
 	counter := 1
 	remainder := 0
 	for number != 0 {
-		remainder = int(number) % int(desiredBase)
-		number = number / int64(desiredBase)
+		remainder = number % desiredBase
+		number = number / desiredBase
 		result += remainder * counter
 		counter *= 10
 	}
